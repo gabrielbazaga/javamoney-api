7a812f9b5dfcd41a0f5793f1cb038e1fad706c30
gabrielbazaga <gabrielbazaga@gmail.com>
2021-03-01 16:38:58 -0300

    Atualizaçao de lancamento

--- src/main/java/com/bazaga/javamoney/api/service/LancamentoService.java
+++ src/main/java/com/bazaga/javamoney/api/service/LancamentoService.java
@@ -1,7 +1,8 @@
 package com.bazaga.javamoney.api.service;
 
-import javax.validation.Valid;
+import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,12 +21,34 @@ public class LancamentoService {
 	@Autowired
 	 private LancamentoRepository lancamentoRepository;
 	
-	public Lancamento salvar(@Valid Lancamento lancamento) {
-		Pessoa pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo()).orElse(null);
-		if(pessoa == null || pessoa.isInativo()) {
-			throw new PessoaInexistenOuInativaException();
-		} 
+	public Lancamento salvar(Lancamento lancamento) {
+		validarPessoa(lancamento);
 		return lancamentoRepository.save(lancamento);
+	}
+
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
+		}
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+		return lancamentoRepository.save(lancamentoSalvo);
+	}
+	
+	private void validarPessoa(Lancamento lancamento) {
+		Optional <Pessoa> pessoa = null;
+		if (lancamento.getPessoa().getCodigo() != null) {
+			pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+		}
+		if(pessoa == null || pessoa.isEmpty() || pessoa.get().isInativo()) {
+			throw new PessoaInexistenOuInativaException();
+		}
+		
+	}
+	
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+		Optional<Lancamento> lancamentoSalvoOpt = lancamentoRepository.findById(codigo);
+		return lancamentoSalvoOpt.orElseThrow(() -> new IllegalArgumentException());
 	}
 
 }
