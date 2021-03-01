51ad6cebbc3225c7fb7db53bd47a599905fca6f8
gabrielbazaga <gabrielbazaga@gmail.com>
2021-03-01 16:00:00 -0300

    Implementando Pesquisa de pessoa com parametros

--- src/main/java/com/bazaga/javamoney/api/repository/PessoaRepository.java
+++ src/main/java/com/bazaga/javamoney/api/repository/PessoaRepository.java
@@ -1,12 +1,13 @@
 package com.bazaga.javamoney.api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bazaga.javamoney.api.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
-	
-	
+	public Page<Pessoa> findByNomeContaining(String name, Pageable pageable);
 
 }
--- src/main/java/com/bazaga/javamoney/api/resource/PessoaResource.java
+++ src/main/java/com/bazaga/javamoney/api/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package com.bazaga.javamoney.api.resource;
 
-import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
@@ -8,6 +7,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,51 +38,35 @@ public class PessoaResource {
 	@Autowired
 	private PessoaService pessoaService;
 	
-	@GetMapping
-	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
-	public List<Pessoa> listar() {
-		return pessoaRepository.findAll();
-	}
-	
 	@Autowired
 	private ApplicationEventPublisher publisher;
 	
 	@PostMapping
 	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
-		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
-		
-		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
-		
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));		
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
-		
+	
 	@GetMapping("/{codigo}")
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
 	public ResponseEntity<?> buscarPeloCodigo(@PathVariable Long codigo) {
 		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
 		return pessoa.isPresent() ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
 	}	
-
+	
 	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@PreAuthorize("hasAuthority('ROLE_REMOVER_PESSOA') and #oauth2.hasScope('write')")
-	public ResponseEntity<Pessoa> deletar(@PathVariable Long codigo) {
+	public void remover(@PathVariable Long codigo) {
 		pessoaRepository.deleteById(codigo);
-		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
-
 	
 	@PutMapping("/{codigo}")
 	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa)
 	{
 		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
-		/*
-		Pessoa pessoaSalva = pessoaRepository.findById(codigo).get();
-		if (pessoaSalva == null) {
-			throw new EmptyResultDataAccessException(1);
-		}
-		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
-		pessoaRepository.save(pessoaSalva);*/  //este comentario foi para a classe PessoaService
 		return ResponseEntity.ok(pessoaSalva);
 	}
 	
@@ -89,4 +75,20 @@ public class PessoaResource {
 	public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
 		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
 	}
+	
+	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
+	public Page<Pessoa> pesquisar(@RequestParam(required = false, defaultValue = "%") String nome, Pageable pageable) {
+		return pessoaRepository.findByNomeContaining(nome, pageable);
+	}
+
+		
+
+
+
+
+	
+
+	
+
 }
